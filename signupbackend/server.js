6271a8a9fa899a111935c233250cca4517209e6a
joycethha <joycethh@outlook.com>
2022-07-25 15:45:16 -0500

    tell the server that we have different  routes to handle different request

--- signupbackend/server.js
+++ signupbackend/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const App = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const routesUrls = require("./routes/routes");
+
 dotenv.config();
 
 mongoose.connect(process.env.DATABSE_ACCESS, () =>
   console.log("database is connected")
 );
+
+App.use("/app", routesUrls);
 App.listen(4000, () => console.log("server is up and running"));

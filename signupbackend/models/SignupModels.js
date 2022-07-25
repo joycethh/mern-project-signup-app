2fdad29504fde6530305b106245a56b3090b952e
joycethha <joycethh@outlook.com>
2022-07-25 13:38:01 -0500

    create mongoose schema

---
+++ signupbackend/models/SignupModels.js
@@ -0,0 +1,27 @@
+const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const signUpTemplate = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    requried: true,
+  },
+  email: {
+    type: String,
+    reqired: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.export = mongoose.model("table", signUpTemplate);

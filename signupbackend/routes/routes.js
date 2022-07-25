8aca76eae5f7c3db85a0d4b73e1108648e22a9b2
joycethha <joycethh@outlook.com>
2022-07-25 13:54:20 -0500

    import the schema so that we can copy the schema and temporarily save the “req.body” data to the schema

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const signUpTemplateCopy = require("../models/SignupModels");
 
 router.post("/signup", (req, res) => {
-  res.send("send");
+  const singnedUpUser = new signUpTemplateCopy({
+    fullName: req.body.fullName,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  singnedUpUser
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;

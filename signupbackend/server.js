aa52de2f0f74352f89e8269f5a1dd41939ccb4fe
joycethha <joycethh@outlook.com>
2022-07-25 15:51:19 -0500

    set up middleware and use bodyparser

--- signupbackend/server.js
+++ signupbackend/server.js
@@ -3,12 +3,15 @@ const App = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const routesUrls = require("./routes/routes");
+const cors = require("cors");
 
 dotenv.config();
 
 mongoose.connect(process.env.DATABSE_ACCESS, () =>
   console.log("database is connected")
 );
-
+//invoke middleware in the App.
+App.use(express.json());
+App.use(cors());
 App.use("/app", routesUrls);
 App.listen(4000, () => console.log("server is up and running"));
